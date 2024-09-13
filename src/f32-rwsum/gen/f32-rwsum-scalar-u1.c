b13b50950838d9124379225e5389f81bf205d36c
PrasadMuleMCW <prasad.mule@multicorewareinc.com>
2024-09-13 15:55:24 +0530

    littlebit optimized reduce window implementation

--- src/f32-rwsum/gen/f32-rwsum-scalar-u1.c
+++ src/f32-rwsum/gen/f32-rwsum-scalar-u1.c
@@ -33,14 +33,6 @@ void xnn_f32_rwsum_ukernel__scalar_u1(
     assert(cols != 0);
     assert(input != NULL);
     assert(output != NULL);
-    assert(0 < params->window_dimensions[0]);
-    assert(0 < params->window_dimensions[1]);
-    assert(0 < params->window_strides[0]);
-    assert(0 < params->window_strides[1]);
-    assert(0 < params->base_dilations[0]);
-    assert(0 < params->base_dilations[1]);
-    assert(0 < params->window_dilations[0]);
-    assert(0 < params->window_dilations[1]);
 
     // padding size
     int32_t pad_row = rows + params->padding[0] + MAX((rows-1), 0) * (params->base_dilations[0] - 1) + params->padding[1];
@@ -49,40 +41,33 @@ void xnn_f32_rwsum_ukernel__scalar_u1(
     // output size 
     int32_t out_row = (pad_row < (params->window_dimensions[0] - 1) * params->window_dilations[0] + 1) ? 0 : FLOOR((pad_row - (params->window_dimensions[0] - 1) * params->window_dilations[0] - 1) / (float)params->window_strides[0]) + 1;
     int32_t out_col = (pad_col < (params->window_dimensions[1] - 1) * params->window_dilations[1] + 1) ? 0 : FLOOR((pad_col - (params->window_dimensions[1] - 1) * params->window_dilations[1] - 1) / (float)params->window_strides[1]) + 1;
-      
-    // slice
-    int32_t win_row = CEIL(((params->window_dimensions[0] - 1) * params->window_dilations[0] + 1) / (float)params->window_dilations[0]);
-    int32_t win_col = CEIL(((params->window_dimensions[1] - 1) * params->window_dilations[1] + 1) / (float)params->window_dilations[1]);
-    
-    for (int32_t out_r = 0; out_r < out_row ; out_r++)
-    {
-        for(int32_t out_c = 0; out_c < out_col; out_c++)
-        {
-            output[out_r * out_col + out_c ] = init_value;  // 0   
-            for(int32_t i = 0; i < win_row; i++)
-            {
-                for(int32_t j = 0; j < win_col; j++)
-                {
-                    int32_t r = out_r * params->window_strides[0] + i * params->window_dilations[0];
-                    int32_t c = out_c * params->window_strides[1] + j * params->window_dilations[1];
+       
+    for (int i = 0; i < out_row; i++) {
+        for (int j = 0; j < out_col; j++) {
+            float sum = init_value;
 
-                    int32_t ir = (r - params->padding[0]) / params->base_dilations[0];
-                    int32_t ic = (c - params->padding[2]) / params->base_dilations[1];
+            for (int k = 0; k < params->window_dimensions[0]; k++) {
+                int win_row = i * params->window_strides[0] + k * params->window_dilations[0];
+                win_row -= params->padding[0];
+                if (win_row < 0 || win_row >= rows * params->base_dilations[0] || win_row % params->base_dilations[0] != 0) {
+                    sum += init_value;
+                    continue;
+                }
+                win_row /= params->base_dilations[0];
 
-                    // reduction    
-                    if( r < params->padding[0] || r >= pad_row - params->padding[1] || c < params->padding[2] || c >= pad_col - params->padding[3])
-                        output[out_r * out_col + out_c ] += init_value;
+                for (int l = 0; l < params->window_dimensions[1]; l++) {
+                    int win_col = j * params->window_strides[1] + l * params->window_dilations[1];
+                    win_col -= params->padding[2];
+                    if (win_col < 0 || win_col >= cols * params->base_dilations[1] || win_col % params->base_dilations[1] != 0) {
+                        sum += init_value;
+                        continue;
+                    }
+                    win_col /= params->base_dilations[1];
 
-                    else if ( 0 <= ir && ir < rows && 0 <= ic  && ic < cols)
-                    {
-                        if ((r - params->padding[0]) % params->base_dilations[0] == 0 && (c - params->padding[2]) % params->base_dilations[1] == 0 )
-                            output[out_r * out_col + out_c ] += input[ ir * cols  + ic];
-                        else 
-                           output[out_r * out_col + out_c ] += init_value; //0;
-                    }                   
-
+                    sum += input[win_row * cols + win_col];
                 }
             }
+            output[i * out_col + j] = sum;
         }
     }
 }
